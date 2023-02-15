f75af4bf3f13fcf3af4bcaf1c5739fabbb05c9c6
kalpana <kalpana@DESKTOP-1D5A275>
2023-02-15 16:51:35 +0530

    first commit

---
+++ association/src/orgtnsifundirectional/student.java
@@ -0,0 +1,73 @@
+package orgtnsifundirectional;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Student")
+public class Student {
+	
+	@Id
+	@Column(name="RollNo")
+	private int rollno;
+	@Column(name="Name")
+	private String name;
+	
+	@OneToOne(cascade=CascadeType.ALL)
+	@JoinColumn(name="Pincode")
+	private Address address;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollno=" + rollno + ", name=" + name + ", address=" + address + "]";
+	}
+
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int rollno, String name, Address address) {
+		super();
+		this.rollno = rollno;
+		this.name = name;
+		this.address = address;
+	}
+	
+	
+	
+
+}
+
+public class student {
+
+}
